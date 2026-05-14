3a17839c225ed6e1237d3b1ba2724350a036970c
Melaku Belew <embd2026gc@gmail.com>
2026-05-15 01:21:42 +0300

    setup express server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import app from "./app.js";
+import env from "./config/env.js";
+import connectDB from "./config/db.js";
+
+const startServer = async()=>{
+  try {
+    await connectDB();
+    app.listen(env.port, ()=>{
+      console.log(`Server is running on port ${env.port}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  };
+};
+
+startServer();
