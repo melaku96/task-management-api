3b543dc4262c5803277e4f8f2d6f75780e10ee71
Melaku Belew <embd2026gc@gmail.com>
2026-05-15 23:12:37 +0300

    configure global error handler middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import env from "./config/env.js";
+import globalErrorHandler from "./shared/middlewares/error.middleware.js";
 
 const app = express();
 //middlewares
@@ -15,8 +16,10 @@ app.use(cookieParser());
 //routes
 
 //404 handler
-
+app.use((req, res)=>{
+  res.status(404).json("Route not found")
+});
 //global error handler
-
+app.use(globalErrorHandler);
 //export
 export default app;
