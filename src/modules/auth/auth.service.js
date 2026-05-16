55d3ccaeaf403c2ecb1f6d49ab2889be28a6b1b6
Melaku Belew <embd2026gc@gmail.com>
2026-05-16 19:34:19 +0300

    add resend verificatiion service

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -4,7 +4,7 @@ import { bcryptHash, cryptoHash } from "../../shared/utils/hashToken.js";
 import { sendEmail } from "../../shared/utils/sendEmail.js";
 import userModel from "../user/user.model.js"
 import crypto from "crypto"
-
+//Register
 export const registerService = async (payload) => {
   const isExist = await userModel.findOne({ email: payload.email });
   if (isExist) {
@@ -32,3 +32,21 @@ export const registerService = async (payload) => {
 
   return {user};
 };
+//Resend
+export const resendVerificationService = async(payload)=>{
+  const user = await userModel.findOne({email:payload.email});
+  if(!user){
+    throw new ApiError("User not found.Please register", 404);
+  };
+  const verificationToken = crypto.randomBytes(32).toString('hex');
+  user.verificationToken = cryptoHash(verificationToken);
+  user.verificationTokenExpire = Date.now()+60*60*1000;
+  await user.save();
+  
+  const verificationLink = `http://localhost:3000/api/auth/verify-email?token=${verificationToken}`;
+
+  const htmTemplete = verifyEmailTemplate(verificationLink);
+  await sendEmail(payload.email, "Verify Your Email", htmTemplete);
+
+  return {user};
+}
