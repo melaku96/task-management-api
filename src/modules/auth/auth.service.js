529f401cab0fe31f9a5c102b302080772f493f8e
Melaku Belew <embd2026gc@gmail.com>
2026-05-16 20:22:20 +0300

    add email verification service

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -10,9 +10,9 @@ export const registerService = async (payload) => {
   if (isExist) {
     if (isExist.isVerified) {
       throw new ApiError("Email already verified. Please login.", 409);
-    }else if (!isExist.isVerified && isExist.verificationTokenExpire > Date.now()) {
+    } else if (!isExist.isVerified && isExist.verificationTokenExpire > Date.now()) {
       throw new ApiError(`Email already registred. Check your email and verify it with in ${Math.ceil((isExist.verificationTokenExpire - Date.now()) / 60000)} minutes `, 401);
-    }else {
+    } else {
       await isExist.deleteOne();
     };
   };
@@ -23,30 +23,45 @@ export const registerService = async (payload) => {
     ...payload,
     password: hashedPassword,
     verificationToken: cryptoHash(verificationToken),
-    verificationTokenExpire: Date.now()+60*60*1000,
+    verificationTokenExpire: Date.now() + 60 * 60 * 1000,
   });
   const verificationLink = `http://localhost:3000/api/auth/verify-email?token=${verificationToken}`;
 
   const htmTemplete = verifyEmailTemplate(verificationLink);
   await sendEmail(payload.email, "Verify Your Email", htmTemplete);
 
-  return {user};
+  return { user };
 };
 //Resend
-export const resendVerificationService = async(payload)=>{
-  const user = await userModel.findOne({email:payload.email});
-  if(!user){
+export const resendVerificationService = async (payload) => {
+  const user = await userModel.findOne({ email: payload.email });
+  if (!user) {
     throw new ApiError("User not found.Please register", 404);
   };
   const verificationToken = crypto.randomBytes(32).toString('hex');
   user.verificationToken = cryptoHash(verificationToken);
-  user.verificationTokenExpire = Date.now()+60*60*1000;
+  user.verificationTokenExpire = Date.now() + 60 * 60 * 1000;
   await user.save();
-  
+
   const verificationLink = `http://localhost:3000/api/auth/verify-email?token=${verificationToken}`;
 
   const htmTemplete = verifyEmailTemplate(verificationLink);
   await sendEmail(payload.email, "Verify Your Email", htmTemplete);
 
-  return {user};
+  return { user };
+};
+//Verify Email
+export const verifyEmailService = async (payload) => {
+  const user = await userModel.findOne({
+    verificationToken: cryptoHash(payload.token),
+    verificationTokenExpire: { $gt: Date.now() },
+  });
+  if (!user) {
+    throw new ApiError("Verification time is expired. Please register again", 403);
+  };
+  user.isVerified = true;
+  user.verificationToken = undefined;
+  user.verificationTokenExpire = undefined;
+
+  await user.save();
 }
