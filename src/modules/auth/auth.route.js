ef37d0993ccbb323d3ad37844f22c16e7600eb18
Melaku Belew <embd2026gc@gmail.com>
2026-05-16 19:48:14 +0300

    add resend verification route endpoint

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import validate from "../../shared/middlewares/validate.middleware.js";
-import { registerSchema } from "./auth.validation.js";
-import { registerController } from "./auth.controller.js";
+import { registerSchema, resendSchema } from "./auth.validation.js";
+import { registerController, resendVerificationController } from "./auth.controller.js";
 
 const router = express.Router();
 //routes
 router.post("/register", validate(registerSchema), registerController);
+router.post("/resend-verification", validate(resendSchema), resendVerificationController);
 
 export default router;
