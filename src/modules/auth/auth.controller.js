4e3056565da7a331142a7bed48974770e0b17cda
Melaku Belew <embd2026gc@gmail.com>
2026-05-16 19:41:07 +0300

    add resend verification link controller

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,11 +1,20 @@
 import { catchAsync } from "../../shared/utils/catchAsync.js";
-import { registerService } from "./auth.service.js";
+import { registerService, resendVerificationService } from "./auth.service.js";
 //Register
 export const registerController = catchAsync(async(req, res)=>{
   const {user} = await registerService(req.body);
   res.status(201).json({
     success: true,
-    message: "You are registered successfully.Verification link is sent to your email. Please verify it with in an hour.",
+    message: "Registered successfully.Verification link is sent to your email. Please verify it with in an hour.",
     user,
-  })
-})
+  });
+});
+//Resend verification
+export const resendVerificationController = catchAsync(async(req, res)=>{
+  const {user} = await resendVerificationService(req.body);
+  res.status(201).json({
+    success: true,
+    message: "Verification link is sent to your email. Please verify it with in an hour.",
+    user,
+  });
+});
