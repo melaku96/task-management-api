f16adfb91f7e46896bddbd0135356bae3e335b11
Melaku Belew <embd2026gc@gmail.com>
2026-05-16 20:29:47 +0300

    configure email verification route endpoint

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import validate from "../../shared/middlewares/validate.middleware.js";
 import { registerSchema, resendSchema } from "./auth.validation.js";
-import { registerController, resendVerificationController } from "./auth.controller.js";
+import { emailVerificationController, registerController, resendVerificationController } from "./auth.controller.js";
 
 const router = express.Router();
 //routes
 router.post("/register", validate(registerSchema), registerController);
 router.post("/resend-verification", validate(resendSchema), resendVerificationController);
+router.get("/verify-email", emailVerificationController);
 
 export default router;
