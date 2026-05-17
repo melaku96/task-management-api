93abe8257a4910b6b7561e58cb70e6329259313b
Melaku Belew <embd2026gc@gmail.com>
2026-05-17 13:13:51 +0300

    add forgot password schema

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -14,3 +14,7 @@ export const loginSchema = z.object({
   email: z.string().email().trim(),
   password: z.string().min(6).trim()
 });
+
+export const forgotSchema = z.object({
+  email: z.string().email().trim(), 
+});
