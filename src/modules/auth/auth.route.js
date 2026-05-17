a873b12eac0380e7282585d062aa1b9767be3fe4
Melaku Belew <embd2026gc@gmail.com>
2026-05-17 10:51:37 +0300

    add login route edpoint

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import validate from "../../shared/middlewares/validate.middleware.js";
-import { registerSchema, resendSchema } from "./auth.validation.js";
-import { emailVerificationController, registerController, resendVerificationController } from "./auth.controller.js";
+import { loginSchema, registerSchema, resendSchema } from "./auth.validation.js";
+import { emailVerificationController, loginController, registerController, resendVerificationController } from "./auth.controller.js";
 
 const router = express.Router();
 //routes
 router.post("/register", validate(registerSchema), registerController);
 router.post("/resend-verification", validate(resendSchema), resendVerificationController);
 router.get("/verify-email", emailVerificationController);
+router.post("/login", validate(loginSchema), loginController);
 
 export default router;
