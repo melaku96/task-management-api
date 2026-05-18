efd8f6c382439dfc2c9949b0bedee3bddc35bba4
Melaku Belew <embd2026gc@gmail.com>
2026-05-18 23:18:39 +0300

    add user role for authorizing token

--- src/shared/utils/generateToken.js
+++ src/shared/utils/generateToken.js
@@ -3,7 +3,7 @@ import env from '../../config/env.js';
 
 export const generateToken = (user)=>{
   return jwt.sign(
-    {_id:user._id},
+    {_id:user._id, role: user.role},
     env.accessTokenSecret,
     {expiresIn:'30m'}
   );
