4530e3220e9dbb7faf52d335aec0f6da0b7e8357
Melaku Belew <embd2026gc@gmail.com>
2026-05-19 22:58:36 +0300

    implement validation for user update

--- src/modules/user/user.route.js
+++ src/modules/user/user.route.js
@@ -3,13 +3,13 @@ import { authMiddleware } from "../../shared/middlewares/auth.middleware.js";
 import { changePasswordController, deleteUserController, getAllUsersController, getCurrentUserController, updateUserController } from "./user.controller.js";
 import { roleMiddleware } from "../../shared/middlewares/role.middleeware.js";
 import validate from "../../shared/middlewares/validate.middleware.js";
-import { changePasswordSchema } from "./user.validation.js";
+import { changePasswordSchema, updateSchema } from "./user.validation.js";
 
 const router = express.Router();
 //routes
 router.get("/me", authMiddleware, getCurrentUserController);
 router.get("/get-all", authMiddleware, roleMiddleware("admin", "manager"), getAllUsersController);
-router.patch("/update", authMiddleware, updateUserController);
+router.patch("/update", authMiddleware,validate(updateSchema), updateUserController);
 router.patch("/change-password", authMiddleware,validate(changePasswordSchema),changePasswordController);
 router.delete("/delete-user", authMiddleware, deleteUserController);
 
