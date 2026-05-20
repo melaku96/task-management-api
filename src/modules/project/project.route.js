ea2ac01ee7c84c94554893174fdeff13116f107d
Melaku Belew <embd2026gc@gmail.com>
2026-05-21 02:11:15 +0300

    add project management endpoints

--- src/modules/project/project.route.js
+++ src/modules/project/project.route.js
@@ -1,11 +1,15 @@
 import express from "express";
 import validate from "../../shared/middlewares/validate.middleware.js";
-import { createProjectSchema } from "./project.validation.js";
-import { createProjectController } from "./project.controller.js";
+import { createProjectSchema, updateProjectSchema } from "./project.validation.js";
+import { createProjectController, deleteProjectController, getAllProjectsController, getSingleProjectsController, updateProjectController } from "./project.controller.js";
 import { authMiddleware } from "../../shared/middlewares/auth.middleware.js";
 
 const router = express.Router();
 //routes
 router.post("/create", authMiddleware, validate(createProjectSchema), createProjectController);
+router.get("/single-project/:id", authMiddleware, getSingleProjectsController);
+router.get("/all-projects", authMiddleware, getAllProjectsController);
+router.patch("/update-project/:id", authMiddleware, validate(updateProjectSchema), updateProjectController);
+router.delete("/delete-project/:id", authMiddleware, deleteProjectController);
 
 export default router;
--- src/modules/project/project.service.js
+++ src/modules/project/project.service.js
@@ -1,10 +1,50 @@
-import projectModel from "./project.model.js"
+import ApiError from "../../shared/errors/ApiError.js";
+import projectModel from "./project.model.js";
 
-export const createProjectService = async(userId, payload)=>{
+export const createProjectService = async (userId, payload) => {
   const project = await projectModel.create({
     ...payload,
     owner: userId,
-    members: [userId]
+    members: [userId],
   });
+  return { project };
+};
+//get progects
+export const getAllProjctsService = async (userId) => {
+  const projects = await projectModel.findOne({ members: userId })
+  .populate("owner", "name email")
+  .sort({createdAt: -1});
+  if (!projects) {
+    throw new ApiError("Project not found", 404);
+  };
+  return { projects };
+};
+//get a single project
+export const getSingleProjctsService = async (projectId) => {
+  const project = await projectModel.findOne({ _id: projectId })
+  .populate("owner", "name email")
+  .populate("members", "name email");
+  if (!project) {
+    throw new ApiError("Project not found", 404);
+  }
+  return { project };
+};
+//update project
+export const updateProjectService = async(projectId,userId,payload)=>{
+  const project = await projectModel.findOneAndUpdate(
+    {_id:projectId, owner: userId},
+    payload,
+    {returnDocument: "after"},
+  );
+   if (!project) {
+    throw new ApiError("Project not found or you can not update it", 404);
+  };
   return {project};
+};
+//delete project
+export const deleteProjectService = async(projectId, userId)=>{
+  const project = await projectModel.findOneAndDelete({_id: projectId, owner:userId});
+   if (!project) {
+    throw new ApiError("Project not found or you can not delete it", 404);
+  };
 };
